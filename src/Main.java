096606e145ea5ed292fcd31eab20b4772c73df5f
krzys <krzysztofmuller1@gmail.com>
2021-09-04 15:11:45 +0200

    przykład.3

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        /* Przykłady wprowadzania tekstu z klawiatury */
 
+        /* Przykłady wprowadzania tekstu z klawiatury */
         /* Ta komenda inicjuje strumień wejsciomy danych */
         Scanner scan = new Scanner(System.in);
 
@@ -23,6 +23,5 @@ public class Main {
         /*  Uwaga wtym wypadku gdy podamy liczbę np. 4 to konsola nie wypisze nam licz-by 5 tylko liczbę 41
             bo będzie łączyla każdy znak od lewej do typu tekstowego */
         System.out.println("Twój tekst to " + tekst + " i liczba zwiększona o 1 to " + liczba + 1);
-
     }
 }
