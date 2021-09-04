a14d22b96b015ead6232cadcbcade711d2907aeb
krzys <krzysztofmuller1@gmail.com>
2021-09-04 22:59:12 +0200

    przykład.4

--- src/Main.java
+++ src/Main.java
@@ -12,22 +12,22 @@ public class Main {
                 "\na = true\tb = false\tc = true");
 
         /* Iloczyn logiczny - AND */
-        System.out.println("a && b: " + (a && b)); // false
-        System.out.println("a && c: " + (a && c)); // true
+        System.out.println("(a && b) : " + (a && b)); // false
+        System.out.println("(a && c) : " + (a && c)); // true
 
         /* Różnica logiczna - OR */
-        System.out.println("a || b: " + (a || b)); // true
-        System.out.println("a || c: " + (a || c)); // true
-        System.out.println("false || b: " + (false || b)); // false
+        System.out.println("(a || b) : " + (a || b)); // true
+        System.out.println("(a || c) : " + (a || c)); // true
+        System.out.println("(false || b) : " + (false || b)); // false
 
 
         /* zaprzeczenie  - NOT */
-        System.out.println("!a: " + !a); // false
-        System.out.println("!b: " + !b); // true
+        System.out.println("!a : " + !a); // false
+        System.out.println("!b : " + !b); // true
 
         /* łączenie operatorów */
-        System.out.println("!(a && b): " + !(a && b)); // true
-        System.out.println("!((a && c) || (!a || b): " + !((a && c) || (!a || b))); // false
+        System.out.println("!(a && b) : " + !(a && b)); // true
+        System.out.println("!((a && c) || (!a || b) : " + !((a && c) || (!a || b))); // false
 
         System.out.println();
 
@@ -41,19 +41,19 @@ public class Main {
                 "\nx = %d\ty = %d\tz = %d\n", x, y, z);
 
         /* porównanie */
-        System.out.println("x == y: " + (x == y)); // false
-        System.out.println("x == z: " + (x == z)); // true
+        System.out.println("(x == y) : " + (x == y)); // false
+        System.out.println("(x == z) : " + (x == z)); // true
 
         /* nierówne */
-        System.out.println("x != y: " + (x != y)); // true
-        System.out.println("x != z: " + (x != z)); // false
+        System.out.println("(x != y) : " + (x != y)); // true
+        System.out.println("(x != z) : " + (x != z)); // false
 
         /* pozostałe operatory */
-        System.out.println("x > y: " + (x > y)); // false
-        System.out.println("x < y: " + (x < y)); // true
-        System.out.println("y >= x: " + (y >= x)); // true
+        System.out.println("(x > y) : " + (x > y)); // false
+        System.out.println("(x < y) : " + (x < y)); // true
+        System.out.println("(y >= x) : " + (y >= x)); // true
 
-        System.out.println("(x > y) || (a && b): " + ((x > y) || (a && c))); // true
+        System.out.println("((x > y) || (a && b)) : " + ((x > y) || (a && c))); // true
 
         /* zad.1 proszę dodać kilka złożonych operacji uzywając kilku operatorów */
     }
