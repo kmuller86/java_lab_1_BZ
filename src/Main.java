44bdce5570cb3f9bc3161522e07090744887580e
barte <zawadzki3@protonmail.com>
2021-11-21 15:40:32 +0100

    ćwiczenie 2 zrobione

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int zmiennaInt = 7;
-        System.out.println("zmienna int = " + zmiennaInt);
+        int zmienna1 = 16;
+        String imie1 = "Piotr";
+        double wiek = 27.11;
+        boolean zmiennaBoolean = true;
+
+        System.out.println(imie1);
+        System.out.println(zmienna1);
+        System.out.println(wiek);
+        System.out.print(zmiennaBoolean);
+
+
+
+        //cw2
+
+        int liczbaA = 7;
+        int liczbaB = 10;
+
+        double liczbaX = 12.5;
+        double liczbaY = 20.3;
+
+
+        System.out.println(liczbaA + liczbaB);
+        System.out.println(liczbaA - liczbaB);
+        System.out.println(liczbaA * liczbaB);
+        System.out.println(liczbaA/liczbaB);
+        System.out.println(liczbaB%liczbaA);
+
         /* ćw.2 */
 
         /*   zad.1 zadeklaruj i zainicjalizuj zmienne różnych typów i wyświetl w konsoli (im więcej tym lepiej ;) ) */
