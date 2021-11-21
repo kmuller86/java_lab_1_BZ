a2dfde704ae9ca97b8c66fa148f98adf39df82b4
barte <zawadzki3@protonmail.com>
2021-11-21 16:39:28 +0100

    ćwiczenie 6 zrobione

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,16 @@ public class Main {
         /* zad1. stworzyć tablicę typu double 5-cio elementową,
         przypisać do niej wartości i wyświetlić */
 
+        int tab1[] = new int[5];
+        tab1[0] = 5;
+        tab1[1]=25;
+        tab1[2]=50;
+        tab1[3]=75;
+        tab1[4]=100;
+        System.out.println(tab1.length);
+        for (int i = 0; i < tab1.length; i++){
+            System.out.println(tab1[i] + " ");
+        }
+
     }
 }
